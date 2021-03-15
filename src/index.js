91f5d40137a5cfe2db29adcd079ec6ed2ad4cfc1
ZloyLis <advtsetting@gmail.com>
2021-03-16 01:39:27 +0300

    task: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+
+    let ersever = String(n).split('').reverse();
+
+    if (n <= 0) {
+        ersever.pop();
+    }
+
+    return ersever.join('');
 }
